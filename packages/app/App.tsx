511b8be78694d18c5c3cd084d919e2556084d9dc
Matheus Santos <matheussm301@gmail.com>
2020-10-13 19:48:53 -0300

    feat: Add app mobile

---
+++ packages/app/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App: React.FC = () => (
+    <View style={styles.container}>
+      <Text>Olá Mundo</Text>
+      <StatusBar style="auto" />
+    </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
