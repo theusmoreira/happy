63e3b34a9cc4f6e021f1a59dff69b841f75049f2
Matheus Santos <matheussm301@gmail.com>
2020-10-14 20:19:01 -0300

    feat: Add error handler

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -4,13 +4,16 @@ import './database/connection';
 
 import express from 'express';
 import path from 'path';
+import 'express-async-errors';
 import Routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
 app.use(express.json());
 app.use(Routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 const PORT = 3333;
 
