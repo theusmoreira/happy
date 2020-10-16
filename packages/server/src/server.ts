a1e8ecb097f6d6a8f991280913df1d4b49f0a0d9
Matheus Santos <matheussm301@gmail.com>
2020-10-16 01:14:47 -0300

    feat: Add cors

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 import './database/connection';
 
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
 import 'express-async-errors';
 import Routes from './routes';
@@ -10,6 +11,7 @@ import errorHandler from './errors/handler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(Routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
